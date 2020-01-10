1b01727ec37d6762790c00b9133f0579b9466409
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-10 17:43:16 -0500

    Map Demo

--- Map Demo/ViewController.swift
+++ Map Demo/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // define latitude and longitude
+        let latitude: CLLocationDegrees = 43.64
+        let longitude: CLLocationDegrees = -79.38
+        
+        // define delta latitude and longitude
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        
+        // define span
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        // define location
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        // define the region
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        // set the region on the map
+        mapView.setRegion(region, animated: true)
+        
+        // adding annotation for the map
+        let annotation = MKPointAnnotation()
+        annotation.title = "Toronto City"
+        annotation.subtitle = "City of Dreams"
+        annotation.coordinate = location
+        mapView.addAnnotation(annotation)
+        
+        // add long press gesture
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(longPress))
+        mapView.addGestureRecognizer(uilpgr)
+    }
+    
+    @objc func longPress(gestureRecognizer: UIGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "place to visit"
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
     }
 
 
